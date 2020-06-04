1a71f5564e21efb14814d6bcccbb0d871173ea6f
SahinDuman <sahinduman247@gmail.com>
2020-06-04 11:26:05 +0200

    tweaked and fixed the sections. Created a Grid for the Video Cards

---
+++ index.js
@@ -0,0 +1,73 @@
+const videos = [
+  {
+    name: 'Kan vi göra det själva?',
+    genre: 'Dokumentär',
+    uri: 'https://player.vimeo.com/video/97210026',
+    description: "Trailer för Kan vi göra det själva? (2015) som har visats på svensk TV, i Riksdagen och inkluderats i Film For Action's lista 'The Top 100 Documentaries We Can Use To Change The World'.",
+    image: require('../assets/thumbnails/kan_vi_gora_det_sjalva.png')
+  }
+  ,
+  {
+    name: 'Omsorg - om framtiden',
+    genre: 'Utbildningsfilm',
+    uri: 'https://player.vimeo.com/video/195768927',
+    description: 'Kortfilm om framtidens äldreomsorg. Producerat på uppdrag av Institutet för framtidsstudier.',
+    image: require('../assets/thumbnails/omsorg.png')
+  },
+  {
+    name: 'Agneta',
+    genre: 'Dokumentär',
+    uri: 'https://player.vimeo.com/video/208637653',
+    description: 'Trailer för dokumentärfilmen Agneta (2018) som hade premiär på Tromsø international film festival.',
+    image: require('../assets/thumbnails/agneta.png')
+  },
+  {
+    name: 'Projekt Framsteg - Champlain Housing Trust',
+    genre: 'Reportagefilm',
+    uri: 'https://player.vimeo.com/video/255924729',
+    description: 'En reportagefilm från Burlington, Vermont om stadens unika system för markägande. Producerad för Projekt framsteg.',
+    image: require('../assets/thumbnails/projekt_framsteg.png')
+  },
+  {
+    name: 'Personalförvärv av företag',
+    genre: 'Reportagefilm',
+    uri: 'https://player.vimeo.com/video/260076744',
+    description: 'En pedagogisk animerad film om hur personalförvärv kan användas som ett alternativ vid generationsskiften i företaget.',
+    image: require('../assets/thumbnails/personalforvarv.png')
+  },
+  {
+    name: 'Magelungen',
+    genre: 'Reportagefilm',
+    uri: 'https://player.vimeo.com/video/274854867',
+    description: 'En kort reportagefilm om det personalägda skolföretaget Magelungen.',
+    image: require('../assets/thumbnails/magelungen.png')
+  },
+  {
+    name: 'Svung - hur ser din drömvärld ut?',
+    genre: 'Reportagefilm',
+    uri: 'https://player.vimeo.com/video/314024590',
+    description: 'En film om för sociala medier om ungdomars stora visioner och drömmar. Producerad för Sveriges Ungdomsråd.',
+    image: require('../assets/thumbnails/dromvarld.png')
+  },
+  {
+    name: 'Svung - organisera dig!',
+    genre: 'Reportagefilm',
+    uri: 'https://player.vimeo.com/video/311184487',
+    description: 'En film för sociala medier om varför ungdomar ska organisera sig. Producerad för Sveriges Ungdomsråd.',
+    image: require('../assets/thumbnails/att_organisera_dig.png')
+  },
+  {
+    name: 'Heaven Group',
+    genre: 'Kortfilm',
+    uri: 'https://player.vimeo.com/video/107875433',
+    description: 'Heaven Group är en kortfilm om en konkurrensutsatt eftervärld. Premiär på Comedy Cluj International Film Festival.',
+    image: require('../assets/thumbnails/heaven_group.png')
+  },
+  {
+    name: 'This is mine',
+    genre: 'Reportagefilm',
+    uri: 'https://player.vimeo.com/video/199698515',
+    description: 'Trailer för en ännu ej producerad film om bostadskrisen i Detroit.',
+    image: require('../assets/thumbnails/this_is_mine.png')
+  }
+]
